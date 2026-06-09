ba2d09cbc4340516807bbbf1e9f7df52d91441b9
joaovictor-18 <joaovictor.oliveira@edu.unifil.br>
2026-06-09 15:47:42 -0300

    fetching

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,3 +1,5 @@
-function fetchProfileData () {
-    const url = 
+async function fetchProfileData () {
+    const url = 'https://raw.githubusercontent.com/joaovictor-18/js-developer-portfolio/refs/heads/main/data/profile.json';
+    const fetching = await fetch(url)
+    return await fetching.json()
 }
